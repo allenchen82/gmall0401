c13dc316a563803e65344e9101a23acd98ec298a
allenchen82 <52658586@qq.com>
2020-05-06 13:55:00 +0800

    modify ch 0506

--- gmall0401-bean/src/main/java/com/atguigu/gmall0401/bean/OrderDetail.java
+++ gmall0401-bean/src/main/java/com/atguigu/gmall0401/bean/OrderDetail.java
@@ -15,6 +15,71 @@ public class OrderDetail implements Serializable {
     @Id
     @Column
     private String id;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setSkuId(String skuId) {
+        this.skuId = skuId;
+    }
+
+    public void setSkuName(String skuName) {
+        this.skuName = skuName;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void setOrderPrice(BigDecimal orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public void setSkuNum(Integer skuNum) {
+        this.skuNum = skuNum;
+    }
+
+    public void setHasStock(String hasStock) {
+        this.hasStock = hasStock;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getSkuId() {
+        return skuId;
+    }
+
+    public String getSkuName() {
+        return skuName;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
+    }
+
+    public Integer getSkuNum() {
+        return skuNum;
+    }
+
+    public String getHasStock() {
+        return hasStock;
+    }
+
     @Column
     private String orderId;
     @Column

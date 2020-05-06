6941586dfcee4b77b93906119841a6d4c515abca
allenchen82 <52658586@qq.com>
2020-05-06 14:45:10 +0800

    modify ch 0506

--- gmall0401-bean/src/main/java/com/atguigu/gmall0401/bean/UserInfo.java
+++ gmall0401-bean/src/main/java/com/atguigu/gmall0401/bean/UserInfo.java
@@ -38,8 +38,73 @@ public class UserInfo implements Serializable {
     private String phoneNum;
     @Column
     private String email;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public void setUserLevel(String userLevel) {
+        this.userLevel = userLevel;
+    }
+
     @Column
     private String headImg;
     @Column
     private String userLevel;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public String getUserLevel() {
+        return userLevel;
+    }
 }
